a7ef90f0111f8404704f5d57e0ca6ffcbf3170bb
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-05-01 00:35:30 +0200

    Create MainActivity view, add ZIP Code check in MainActivity and change color palette

--- app/src/main/java/com/davidmendozamartinez/ad340/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/ad340/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.davidmendozamartinez.ad340
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipCodeEditText: EditText = findViewById(R.id.zipCodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            val zipCode: String = zipCodeEditText.text.toString()
+
+            if (zipCode.length != 5) {
+                Toast.makeText(this, R.string.zip_code_entry_error, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, zipCode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
