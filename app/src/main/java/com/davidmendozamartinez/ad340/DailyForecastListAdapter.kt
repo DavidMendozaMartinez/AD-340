c19ee5f8a7e944ee68645b877b24d6028ba30c76
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-06-23 14:31:17 +0200

    Add ViewBinding in DailyForecastListAdapter

--- app/src/main/java/com/davidmendozamartinez/ad340/DailyForecastListAdapter.kt
+++ app/src/main/java/com/davidmendozamartinez/ad340/DailyForecastListAdapter.kt
@@ -1,38 +1,30 @@
 package com.davidmendozamartinez.ad340
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.davidmendozamartinez.ad340.api.DailyForecast
+import com.davidmendozamartinez.ad340.databinding.ItemDailyForecastBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DailyForecastViewHolder(
-    view: View,
+    private val binding: ItemDailyForecastBinding,
     private val tempDisplaySettingManager: TempDisplaySettingManager,
-    private val locale: Locale
-) : RecyclerView.ViewHolder(view) {
-    private val forecastIcon = view.findViewById<ImageView>(R.id.forecastIcon)
-    private val tempText = view.findViewById<TextView>(R.id.tempText)
-    private val descriptionText = view.findViewById<TextView>(R.id.descriptionText)
-    private val dateText = view.findViewById<TextView>(R.id.dateText)
+    private val dateFormat: SimpleDateFormat
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(dailyForecast: DailyForecast) {
-        tempText.text = formatTempForDisplay(
+        binding.tempText.text = formatTempForDisplay(
             dailyForecast.temp.max, tempDisplaySettingManager.getTempDisplaySetting()
         )
-        descriptionText.text = dailyForecast.weather[0].description
-        val dateFormat = SimpleDateFormat(dateText.context.getString(R.string.date_format), locale)
-        dateText.text = dateFormat.format(Date(dailyForecast.date * 1000))
-
+        binding.descriptionText.text = dailyForecast.weather[0].description
+        binding.dateText.text = dateFormat.format(Date(dailyForecast.date * 1000))
         val iconId = dailyForecast.weather[0].icon
-        forecastIcon.load("http://openweathermap.org/img/wn/${iconId}@2x.png")
+        binding.forecastIcon.load("http://openweathermap.org/img/wn/${iconId}@2x.png")
     }
 }
 
@@ -43,23 +35,22 @@ class DailyForecastListAdapter(
 
     companion object {
         val DIFF_CONFIG = object : DiffUtil.ItemCallback<DailyForecast>() {
-            override fun areItemsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean {
-                return oldItem === newItem
-            }
+            override fun areItemsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean =
+                oldItem === newItem
 
             override fun areContentsTheSame(
                 oldItem: DailyForecast,
                 newItem: DailyForecast
-            ): Boolean {
-                return oldItem == newItem
-            }
+            ): Boolean = oldItem == newItem
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyForecastViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent, false)
-        return DailyForecastViewHolder(itemView, tempDisplaySettingManager, Locale.getDefault())
+        val binding =
+            ItemDailyForecastBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val dateFormat =
+            SimpleDateFormat(parent.context.getString(R.string.date_format), Locale.getDefault())
+        return DailyForecastViewHolder(binding, tempDisplaySettingManager, dateFormat)
     }
 
     override fun onBindViewHolder(holder: DailyForecastViewHolder, position: Int) {
